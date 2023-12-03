1994eb1f59a34d4ea7af9df83ecc250094da76f7
gugaberidze <149968410+gugaberidze@users.noreply.github.com>
2023-12-03 23:30:34 +0400

    Add files via upload

---
+++ day 20/homework/leasson20.py
@@ -0,0 +1,38 @@
+
+# first
+def find_needle(haystack):
+    index= haystack .index("needle")
+    if"needle" in haystack:
+        return f"found the needle at position {index}
+
+#  second
+ def sum_array(a):
+     if not a:
+         return 0
+     else:
+         return sum(a)
+
+
+
+# third
+def sum_array(a):
+    if not a:
+        return 0
+    else:
+        return sum(a)
+
+
+#fourth
+def are_you_playing_banjo(name):
+    if name[0] in "rR":
+        return name + " plays banjo"
+    else:
+        return name + " does not play banjo"
+
+
+#fifth
+def invert(lst):
+    my_arr = []
+    for i in lst:
+        my_arr.append(i * -1)
+    return my_arr
